c3537cd19853ba23f346a97bdbf11ad774264e0c
Lalit Thapa <mlalitthapa@gmail.com>
2017-03-12 10:08:23 +0545

    typecasting the service methods and parameters

--- app/http-requests/posts/post.service.ts
+++ app/http-requests/posts/post.service.ts
@@ -1,6 +1,8 @@
 import {Http} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {Injectable} from 'angular2/core';
+import {Post} from './post';
 
 @Injectable()
 export class PostService {
@@ -11,12 +13,12 @@ export class PostService {
 
     }
 
-    getPosts(){
+    getPosts() : Observable<Post[]> {
         return this._http.get(this._url)
                     .map(res => res.json());
     }
 
-    createPost(post){
+    createPost(post: Post){
         return this._http.post(this._url, JSON.stringify(post))
                     .map(res => res.json());
     }
