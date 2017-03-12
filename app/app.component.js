15d22fa230206a2fd2ed00d7591b82f3b91f994d
Lalit Thapa <mlalitthapa@gmail.com>
2017-03-12 09:55:30 +0545

    simple get request

--- app/app.component.js
+++ app/app.component.js
@@ -1,5 +1,4 @@
-/// <reference path="../typings/tsd.d.ts" />
-System.register(['angular2/core', 'angular2/common', 'rxjs/Rx', 'rxjs/add/operator/debounceTime', 'rxjs/add/operator/map'], function(exports_1) {
+System.register(['angular2/core', './http-requests/posts/post.service', 'angular2/http'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -9,115 +8,33 @@ System.register(['angular2/core', 'angular2/common', 'rxjs/Rx', 'rxjs/add/operat
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1, Rx_1;
+    var core_1, post_service_1, http_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (common_1_1) {
-                common_1 = common_1_1;
+            function (post_service_1_1) {
+                post_service_1 = post_service_1_1;
             },
-            function (Rx_1_1) {
-                Rx_1 = Rx_1_1;
-            },
-            function (_1) {},
-            function (_2) {}],
+            function (http_1_1) {
+                http_1 = http_1_1;
+            }],
         execute: function() {
             AppComponent = (function () {
-                function AppComponent(fb) {
-                    this.form = fb.group({
-                        search: []
-                    });
-                    var search = this.form.find('search');
-                    search.valueChanges
-                        .debounceTime(400)
-                        .map(function (str) { return str.replace(' ', '-'); })
-                        .subscribe(function (x) { return console.log(x); });
-                    // this.travelSearch();
-                    // this.timer()
-                    // this.threads()
-                    // this.errorHandling()
-                    // this.retry()
-                    // this.catchingError()
-                    this.timeouts();
+                function AppComponent(_postService) {
+                    this._postService = _postService;
+                    this._postService.getPosts()
+                        .subscribe(function (posts) { return console.log(posts); });
                 }
-                ;
-                AppComponent.prototype.travelSearch = function () {
-                    var startDates = [];
-                    var startDate = new Date();
-                    for (var day = -1; day <= 2; day++) {
-                        var date = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + day);
-                        startDates.push(date);
-                    }
-                    Rx_1.Observable
-                        .fromArray(startDates)
-                        .map(function (date) {
-                        console.log("Getting deals for date " + date);
-                        return [1, 2, 3];
-                    })
-                        .subscribe(function (x) { return console.log(x); });
-                };
-                AppComponent.prototype.timer = function () {
-                    var observable = Rx_1.Observable.interval(1000);
-                    observable
-                        .flatMap(function (x) {
-                        console.log("calling the server");
-                        return Rx_1.Observable.of([1, 2, 3]);
-                    })
-                        .subscribe(function (news) { return console.log(news); });
-                };
-                AppComponent.prototype.threads = function () {
-                    var userstream = Rx_1.Observable.of({
-                        userId: 1, username: 'Lalit'
-                    }).delay(2000);
-                    var tweetstream = Rx_1.Observable.of([1, 2, 3]).delay(1500);
-                    Rx_1.Observable
-                        .forkJoin(userstream, tweetstream)
-                        .map(function (joined) { return new Object({
-                        user: joined[0],
-                        tweets: joined[1]
-                    }); })
-                        .subscribe(function (result) { return console.log(result); });
-                };
-                AppComponent.prototype.errorHandling = function () {
-                    var observable = Rx_1.Observable.throw(new Error("Something went wrong."));
-                    observable.subscribe(function (x) { return console.log(x); }, function (error) { return console.error(error); });
-                };
-                AppComponent.prototype.retry = function () {
-                    var counter = 0;
-                    var ajaxCall = Rx_1.Observable.of('url')
-                        .flatMap(function () {
-                        if (++counter < 2)
-                            return Rx_1.Observable.throw(new Error('Request failed.'));
-                        return Rx_1.Observable.of([1, 2, 3]);
-                    });
-                    ajaxCall
-                        .retry(3)
-                        .subscribe(function (x) { return console.log(x); }, function (error) { return console.error(error); });
-                };
-                AppComponent.prototype.catchingError = function () {
-                    var remoteDataStream = Rx_1.Observable.of([2, 4, 5]);
-                    remoteDataStream
-                        .catch(function (err) {
-                        var localDataStream = Rx_1.Observable.of([1, 2, 3]);
-                        return localDataStream;
-                    })
-                        .subscribe(function (x) { return console.log(x); });
-                };
-                AppComponent.prototype.timeouts = function () {
-                    var remoteDataStream = Rx_1.Observable.of([1, 2, 3]).delay(5000);
-                    remoteDataStream
-                        .timeout(2000)
-                        .subscribe(function (x) { return console.log(x); }, function (error) { return console.error(error); });
-                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <form [ngFormModel]=\"form\">\n            <input type=\"text\" ngControl=\"search\" class=\"form-control\">\n        </form>\n    "
+                        template: "",
+                        providers: [post_service_1.PostService, http_1.HTTP_PROVIDERS]
                     }), 
-                    __metadata('design:paramtypes', [common_1.FormBuilder])
+                    __metadata('design:paramtypes', [post_service_1.PostService])
                 ], AppComponent);
                 return AppComponent;
             })();
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,149 +1,17 @@
-/// <reference path="../typings/tsd.d.ts" />
-
-import {Component} from 'angular2/core'
-import {FormBuilder, ControlGroup} from 'angular2/common'
-import {Observable} from 'rxjs/Rx'
-import 'rxjs/add/operator/debounceTime'
-import 'rxjs/add/operator/map'
+import {Component} from 'angular2/core';
+import {PostService} from './http-requests/posts/post.service'
+import {HTTP_PROVIDERS} from 'angular2/http';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <form [ngFormModel]="form">
-            <input type="text" ngControl="search" class="form-control">
-        </form>
-    `
+    template: ``,
+    providers: [PostService, HTTP_PROVIDERS]
 })
 export class AppComponent {
     
-    form: ControlGroup;
-
-    constructor(fb : FormBuilder){
-        this.form = fb.group({
-            search: []
-        });
-
-        var search = this.form.find('search');
-
-        search.valueChanges
-            .debounceTime(400)
-            .map(str => (<String>str).replace(' ', '-'))
-            .subscribe(x => console.log(x));
-
-        // this.travelSearch();
-        // this.timer()
-        // this.threads()
-        // this.errorHandling()
-        // this.retry()
-        // this.catchingError()
-        this.timeouts()
-    };
-
-    travelSearch(){
-
-        var startDates = [];
-        var startDate = new Date();
-
-        for(var day = -1; day <= 2; day++){
-            var date = new Date(
-                startDate.getFullYear(),
-                startDate.getMonth(),
-                startDate.getDate() + day
-            );
-            startDates.push(date);
-        }
-
-        Observable
-            .fromArray(startDates)
-            .map(date => {
-                console.log("Getting deals for date " + date);
-                return [1, 2, 3];
-            })
-            .subscribe(x => console.log(x))
-
-    }
-
-    timer(){
-
-        var observable = Observable.interval(1000);
-        observable
-            .flatMap(x => {
-                console.log("calling the server")
-                return Observable.of([1, 2, 3]);
-            })
-            .subscribe(news => console.log(news));
-
-    }
-
-    threads(){
-        var userstream = Observable.of({
-            userId: 1, username: 'Lalit'
-        }).delay(2000);
-
-        var tweetstream = Observable.of([1, 2, 3]).delay(1500);
-
-        Observable
-            .forkJoin(userstream, tweetstream)
-            .map(joined => new Object({
-                user: joined[0],
-                tweets: joined[1]
-            }))
-            .subscribe(result => console.log(result));
-    }
-
-    errorHandling(){
-        var observable = Observable.throw(new Error("Something went wrong."))
-        observable.subscribe(
-            x => console.log(x),
-            error => console.error(error)
-        );
-    }
-
-    retry(){
-
-        var counter = 0;
-
-        var ajaxCall = Observable.of('url')
-            .flatMap(() => {
-                if(++counter < 2)
-                    return Observable.throw(new Error('Request failed.'))
-
-                return Observable.of([1, 2, 3]);
-            })
-
-        ajaxCall
-            .retry(3)
-            .subscribe(
-                x => console.log(x),
-                error => console.error(error)
-            );
-
-    }
-
-    catchingError(){
-
-        var remoteDataStream = Observable.of([2, 4, 5])
-
-        remoteDataStream
-            .catch(err => {
-                var localDataStream = Observable.of([1, 2, 3])
-                return localDataStream
-            })
-            .subscribe(x => console.log(x))
-
-    }
-
-    timeouts(){
-
-        var remoteDataStream = Observable.of([1, 2, 3]).delay(5000)
-
-        remoteDataStream
-            .timeout(2000)
-            .subscribe(
-                x => console.log(x),
-                error => console.error(error)
-            )
-
+    constructor(private _postService: PostService){
+        this._postService.getPosts()
+            .subscribe(posts => console.log(posts));
     }
 
 }
