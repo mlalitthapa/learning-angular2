168c03d49ae4dd4a62196b0da023f7f63894b950
Lalit Thapa <mlalitthapa@gmail.com>
2017-03-12 13:01:31 +0545

    route outlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {RouteConfig} from 'angular2/router';
+import {RouteConfig, RouterOutlet} from 'angular2/router';
 
 import {AlbumsComponent} from './albums.component';
 import {ContactComponent} from './contact.component';
@@ -11,7 +11,8 @@ import {ContactComponent} from './contact.component';
 ])
 @Component({
     selector: 'my-app',
-    templateUrl: '/app/app.component.html'
+    templateUrl: '/app/app.component.html',
+    directives: [RouterOutlet]
 })
 export class AppComponent {
 }
