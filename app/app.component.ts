467ff035a435fa4eef773e90aff9cee595dea4d2
Lalit Thapa <mlalitthapa@gmail.com>
2017-03-14 21:41:22 +0545

    clear spa file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,8 @@
 import {Component} from 'angular2/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
-import {AlbumComponent} from './album.component';
-import {AlbumsComponent} from './albums.component';
-import {ContactComponent} from './contact.component';
-
-@RouteConfig([
-    { path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true },
-    { path:'albums/:id', name: 'Album', component: AlbumComponent },
-    { path: '/contact', name: 'Contact', component: ContactComponent },
-    { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
-])
 @Component({
     selector: 'my-app',
-    templateUrl: '/app/app.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    template: 'Hello'    
 })
 export class AppComponent {
 }
